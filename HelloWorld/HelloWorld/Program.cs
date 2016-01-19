48c670c1a4780d6379cf23dfd3961fd152cc14a4
Cristian Ramirez <cristian.ramirez@uhispano.ac.cr>
2016-01-18 21:22:30 -0600

    Version 1.0
    Hello World v.1.0

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //My First program in C#
+            Console.WriteLine("Cristian Ramírez\n\n\nPress Any Key to Exit...");
+            Console.ReadKey();
+        }
+    }
+}
