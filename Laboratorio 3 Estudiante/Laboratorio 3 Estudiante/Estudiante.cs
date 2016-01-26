8498f09cd662ddea4696bcbc04b83f00d1fe7d27
Cristian Ramirez <cristian.ramirez@uhispano.ac.cr>
2016-01-26 13:58:51 -0600

    Laboratorios Form y Clase Estudiante
    Clase 3 programacion 3 - Lunes noche

---
+++ Laboratorio 3 Estudiante/Laboratorio 3 Estudiante/Estudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_Estudiante
+{
+    class Estudiante
+    {
+
+        // Field
+        public string name;
+        // Constructor that takes no arguments.
+        public Estudiante()
+        {
+            name = "unknown";
+        }
+        // Constructor that takes one argument.
+        public Estudiante(string nm)
+        {
+            name = nm;
+        }
+        // Method
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+    }
+
+}
---
+++ Laboratorio 3 Estudiante/Laboratorio 3 Estudiante/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_Estudiante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Call the constructor that has no parameters.
+            Estudiante person1 = new Estudiante();
+            Console.WriteLine(person1.name);
+            person1.SetName("John Smith");
+            Console.WriteLine(person1.name);
+            // Call the constructor that has one parameter.
+            Estudiante person2 = new Estudiante("Sarah Jones");
+            Console.WriteLine(person2.name);
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
